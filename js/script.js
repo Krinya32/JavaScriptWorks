8e2b269014821d340a7877b4c496daabce026f17
Vladimir Krinitsky <krinya32@mail.ru>
2022-01-16 13:54:10 +0300

    add cycle,conditions

--- js/script.js
+++ js/script.js
@@ -1,28 +1,65 @@
 "use strict";
 
-const numberOFFilms = +prompt('How many fils are you watched','');
+const numberOFFilms = +prompt('Сколько фильмов вы уже посмотрели','');
 
 const personalMovieDB = {
-  count:numberOFFilms,
+  count: numberOFFilms,
   movies: {},
   actors: {},
   genres: [],
   privat: false
 };
 
-// загрузить из файла .lang язык
-// if lang == ru 
-// отображаем русский язык
-// else 
-// отображаем английский
+for (let i = 0; i < 2; i++) {
+    const a = prompt ('Один из последних просмотренных фильмов?', '');
+    const b = prompt ('На сколько оцените его?', '');
 
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a]  = b;
+        console.log ('done');
+    } else {
+        console.log ('error');
+        i--;
+    }
+}
 
-const a = prompt('Один из просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его', ''),
-      c = prompt('Один из просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его', '');
+if (personalMovieDB.count < 10) {
+    console.log ('Просмотрено довольно таки мало фильмов');
 
-personalMovieDB.movies[a]  = b;
-personalMovieDB.movies[c]  = d;
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+    console.log ('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log ('Вы киноман');    
+} else {
+    console.log ('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
+
+
+
+
+
+// let message = prompt ('Ваш логин?', '');
+// if (message == 'Админ' ) {
+//     let password = prompt ('Ваш пароль', '');
+//     if (password == 'Я главный') {
+//     alert ('Hello');
+//     } else if (password == '' || password == null) {
+//     alert ('Отменено');
+//     } else {
+//     alert ('Неверный пароль');
+//     }
+// } else if (message == '' || message == null) {
+//     alert( 'Отменено' );
+// } else {
+//     alert( "Я вас не знаю" );
+// }
+
+// let i = 2; 
+//     for (i=2; i<11; i++) {
+//         if (i % 2 == 1) continue;
+//         console.log (i);
+// }
+
+
